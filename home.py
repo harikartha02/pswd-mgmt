d574bd295406118375d52a2e087f617e981b12b3
Hari Kartha <karthahari2002@gmail.com>
2024-04-19 02:40:02 +0530

    home page commit

---
+++ home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Password Management System",
+    page_icon="👋",
+)
+
+st.write("Secure Password Management System")
+
+#st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    The data is encrypted and stored both locally and on the cloud
+
+    **👈 Select a feature from the sidebar** to use the product
+    
+    Manager Feature helps in updating your database of stored passwords
+    and fetching data from them.
+"""
+)
