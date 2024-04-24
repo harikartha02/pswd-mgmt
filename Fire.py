01d0f0f67925aee1267154f7a16d86ed2ab3d8dc
Hari Kartha <karthahari2002@gmail.com>
2024-04-24 11:39:06 +0530

    firebase commit

---
+++ Fire.py
@@ -0,0 +1,16 @@
+# fire.py
+
+def initialize_firebase():
+    import firebase_admin
+    from firebase_admin import credentials, storage
+
+    # Initialize Firebase Admin
+    cred = credentials.Certificate('C:/Users/karth/Downloads/new-proj-366e3-firebase-adminsdk-obezw-0aaeeae22f.json')
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': 'new-proj-366e3.appspot.com'
+    })
+
+    # Get a reference to the storage service
+    bucket = storage.bucket()
+
+    return bucket
