e07b4139c29a402002b3ef76a4aba443cc869fac
Hari Kartha <karthahari2002@gmail.com>
2024-04-24 11:43:31 +0530

    home page push

--- home.py
+++ home.py
@@ -1,21 +1,41 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Password Management System",
-    page_icon="👋",
-)
 
-st.write("Secure Password Management System")
+from Fire import initialize_firebase
 
-#st.sidebar.success("Select a demo above.")
+# Call initialize_firebase function when needed
+#firebase_bucket = initialize_firebase()
 
-st.markdown(
-    """
-    The data is encrypted and stored both locally and on the cloud
+# Now you can use the firebase_bucket reference to interact with Firebase Storage
 
-    **👈 Select a feature from the sidebar** to use the product
-    
-    Manager Feature helps in updating your database of stored passwords
-    and fetching data from them.
-"""
-)
+
+
+
+# Page Title and Header
+st.title("Password Manager")
+st.header("Welcome to our Password Manager App")
+
+# Introduction
+st.write("""
+         The password manager app helps you securely store and manage your passwords for various websites and services.
+         Say goodbye to the hassle of remembering multiple passwords or writing them down on sticky notes!
+         """)
+
+# Key Features
+st.header("Key Features")
+st.write("""
+         - Secure Password Storage: Safely store all your passwords in one place.
+         - Encryption: Your passwords are encrypted using AES-128 to ensure maximum security.
+         - Easy Access: Access your passwords anytime, anywhere, from any device.
+         - Cloud Sync: Sync your passwords across multiple devices for seamless access.
+         - User-Friendly Interface: Intuitive and easy-to-use interface for a hassle-free experience.
+         """)
+
+# Get Started
+st.header("Get Started")
+st.write("""
+         Ready to take control of your passwords? Sign up now and start managing your passwords securely!
+         Use the Navigation Bar to test out different features of the project.
+         """)
+
+
